882024b179c1882966b10182bf04e5c873bd4bba
RomarioAB <alularomario@gmail.com>
2023-09-14 14:00:20 +0100

    test

---
+++ src/spotify.js
@@ -0,0 +1,3 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+// ^ This is where the user goes to when logging in.
+export const authEndpoint = "https://accounts.spotify.com/authorize";
