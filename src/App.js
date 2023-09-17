62ed513890d5cd2915d1dc72b31e9d3d78bcf3d7
Romario <zk019806@student.reading.ac.uk>
2023-09-17 02:39:31 +0100

    context API data, layering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -13,24 +17,24 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)  //step 2: store access token in the state
+      setToken(_token);  //step 2: store access token in the state
+
+      spotify.setAccessToken(_token); //allows me to communicate between the spotify api and React 
+      
+      spotify.getMe().then(user => {
+        console.log("person", user);
+      });
     }
 
-    console.log('I HAVE A TOKEN >>>', token)  
+    console.log('I HAVE A TOKEN >>>', token);  
   }, []);
 
   return (
     // BEM in order to name your class files...
     // if there's token = render logged in message
-    <div className="app">
-      {
-        token ? (
-          <h1>I am logged in</h1>
-        ) : (
-          <Login />
-        )
-      }
-    </div>
+    <div className="app">{token ? 
+      <Player />
+      : <Login />}</div>
   );
 }
 
