bcc4d64678bde2d7e95c461f3a25a3b084266a40
Romario <zk019806@student.reading.ac.uk>
2023-09-19 00:19:32 +0100

    footer design css

--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
     playing: false,
     item: null,
     //REMOVE after finised developing because when adding your token, you're already authorised.
-    //token: 'BQCtKNd48UuP9nJi-vdkb4RVMyfIlfs58W5bK3PPEYbFAyeZHMve8smqTlLBuRR_bvyBgQHyh-XhaZpw0xRvN1HvBgR2bDkddB8H2OZsNUa3mThdNboOO-7xzqLu1tf3gVpZL7c9oVsHRVMoxWvPX6fgNVltRFPSncbf4r7g7QwEai6H7JMD5I1B1CRvnsHAyaHySzyoUbJAzxfQ',
+    token: 'BQCtKNd48UuP9nJi-vdkb4RVMyfIlfs58W5bK3PPEYbFAyeZHMve8smqTlLBuRR_bvyBgQHyh-XhaZpw0xRvN1HvBgR2bDkddB8H2OZsNUa3mThdNboOO-7xzqLu1tf3gVpZL7c9oVsHRVMoxWvPX6fgNVltRFPSncbf4r7g7QwEai6H7JMD5I1B1CRvnsHAyaHySzyoUbJAzxfQ',
 };
 
 const reducer = (state, action) => {
