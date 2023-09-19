5a2f156de1bf5bdf8077950303cbb116c3769452
Romario <zk019806@student.reading.ac.uk>
2023-09-20 00:52:48 +0100

    initial firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAAuxf3_b4JIVy4M9F2daudgF37ujvM-4M",
+    authDomain: "spotify-clone-a2ddc.firebaseapp.com",
+    projectId: "spotify-clone-a2ddc",
+    storageBucket: "spotify-clone-a2ddc.appspot.com",
+    messagingSenderId: "958167107647",
+    appId: "1:958167107647:web:22b73045637eb50594908a",
+    measurementId: "G-SC9V175CJC"
+  };
